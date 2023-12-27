af2af9028ee5b6efef4227d2c38beba77586535e
NikolayZemelko <zemelko1989@gmail.com>
2023-12-27 21:07:30 +0300

    Change serializers
    1) Add full_name field with SerializerMethidField to Supervisor and
       Employee serializers
    2) Change amount of SupervisorSerializer fields for two (id, full_name)
    3) Add job_title field -> obj.get_job_title_display()

--- employees/serializers.py
+++ employees/serializers.py
@@ -3,14 +3,28 @@ from .models import Employee
 
 
 class SupervisorSerializer(serializers.ModelSerializer):
+
+    full_name = serializers.SerializerMethodField()
+
+    def get_full_name(self, obj):
+        return obj.__str__()
+
     class Meta:
         model = Employee
-        exclude = ("date_created",)
+        fields = ("id", "full_name")
 
 
 class EmployeeSerializer(serializers.ModelSerializer):
     supervisor = SupervisorSerializer(many=False, read_only=True)
     photo = serializers.ImageField()
+    full_name = serializers.SerializerMethodField()
+    job_title = serializers.SerializerMethodField()
+
+    def get_job_title(self, obj):
+        return obj.get_job_title_display()
+
+    def get_full_name(self, obj):
+        return obj.__str__()
 
     class Meta:
         model = Employee
